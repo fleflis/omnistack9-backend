e9a07efd7f67b43fb7d47da6620c47fb51f05bea
Christian Sanches <fleflis@endless-point02.local>
2019-10-01 20:55:43 -0300

    spot creation and file handling

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -1,7 +1,28 @@
+import Spot from '../models/Spot'
+import User from '../models/User'
+
 module.exports = {
     async store(req,res) {
-        console.log(req.body)
-        console.log(req.file)
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        const { userId } = req.headers;
+
+        const user = await User.findById(userId)
+
+        if (!user) {
+            return res
+                .status(400)
+                .json({ error : 'Usuário não existe.'})
+        }
+
+        const spot = await Spot.create({
+            user: userId,
+            thumbnail: filename,
+            company,
+            techs: techs.split(',').map(tech => tech.trim()),
+            price
+        })
+
         return res.json({ ok : true})
     }
 }
