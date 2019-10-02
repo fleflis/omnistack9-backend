b8665ba0dd1050b22bb95c22c1a5fae20f8cbb36
Christian Sanches <fleflis@endless-point02.local>
2019-10-01 21:08:51 -0300

    dashboard controller

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,11 @@
+import Spot from '../models/Spot';
+
+module.exports = {
+    async show(req, res) {
+        const {userId} = req.headers;
+        
+        const spots = await Spot.find({ user:userId })
+        
+        return res.json(spots)
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import uploadConfig from './config/upload';
 // Controllers
 import SessionController from './controllers/SessionController';
 import SpotController from './controllers/SpotController';
+import DashboardController from './controllers/DashboardController';
 
 const routes = Router();
 const upload = multer(uploadConfig)
@@ -16,5 +17,7 @@ const upload = multer(uploadConfig)
 routes.post('/sessions', SessionController.store);
 routes.post('/spots', upload.single('thumbnail') , SpotController.store);
 routes.get('/spots', SpotController.index);
+routes.get('/dashboard', DashboardController.show);
+
 
 module.exports = routes;
